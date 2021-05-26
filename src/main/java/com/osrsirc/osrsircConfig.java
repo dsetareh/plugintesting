00268352d8e017ea1f28ce82b474e7a53d01c840
Daniel Setareh <alisetareh512@gmail.com>
2021-05-26 17:35:57 -0400

    better config, plugin reloads config on change

--- src/main/java/com/osrsirc/osrsircConfig.java
+++ src/main/java/com/osrsirc/osrsircConfig.java
@@ -3,16 +3,93 @@ package com.osrsirc;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
-@ConfigGroup("example")
+@ConfigGroup("osrsircConfig")
 public interface osrsircConfig extends Config
 {
-	@ConfigItem(
-		keyName = "fumo",
-		name = "Enable Fumo",
-		description = "toggle fumo"
+	@ConfigSection(
+			name = "Connection Config",
+			description = "IRC Client Configuration",
+			position = 0,
+			closedByDefault = true
 	)
-	default boolean fumo()
+	String connConfig = "connConfig";
+
+	@ConfigItem(
+			keyName = "ircHostname",
+			name = "Host",
+			description = "IRC Server Address",
+			section = connConfig,
+			position = 0
+	)
+	default String ircHostname()
+	{
+		return "irc.oftc.net";
+	}
+
+	@ConfigItem(
+			keyName = "ircPort",
+			name = "Port",
+			description = "IRC Server Port",
+			section = connConfig,
+			position = 1
+	)
+	default int ircPort()
+	{
+		return 6667;
+	}
+
+	@ConfigItem(
+			keyName = "ircChannel",
+			name = "Channel",
+			description = "IRC Channel Name",
+			section = connConfig,
+			position = 2
+	)
+	default String ircChannel()
+	{
+		return "#rs_memes_ayy_lmao";
+	}
+
+	@ConfigItem(
+			keyName = "ircNick",
+			name = "IRC Nick",
+			description = "IRC Nickname",
+			section = connConfig,
+			position = 3
+	)
+	default String ircNick()
+	{
+		return "sneedscape";
+	}
+
+	@ConfigItem(
+			keyName = "syncPublicChat",
+			name = "Public Chat",
+			description = "Whether to send Public Chat messages to IRC"
+	)
+	default boolean syncPublicChat()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "syncFriendsChat",
+			name = "Friends Chat",
+			description = "Whether to send Friends Chat messages to IRC"
+	)
+	default boolean syncFriendsChat()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "syncClanChat",
+			name = "Clan Chat",
+			description = "Whether to send Clan Chat messages to IRC"
+	)
+	default boolean syncClanChat()
 	{
 		return true;
 	}
--- src/main/java/com/osrsirc/osrsircPlugin.java
+++ src/main/java/com/osrsirc/osrsircPlugin.java
@@ -10,57 +10,103 @@ import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
+
 import net.engio.mbassy.listener.Handler;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Fumo"
+	name = "Reimu Fumo (Real)"
 )
 public class osrsircPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
-	org.kitteh.irc.client.library.Client ircClient;
-
 	@Inject
 	private osrsircConfig config;
+
+
+	private org.kitteh.irc.client.library.Client ircClient;
+
 
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "startup", null);
-		ircClient = org.kitteh.irc.client.library.Client.builder().nick("obamafan69").server().port(6667, org.kitteh.irc.client.library.Client.Builder.Server.SecurityType.INSECURE).host("irc.oftc.net").then().buildAndConnect();
-		ircClient.addChannel("#rs_memes_ayy_lmao");
-		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "connected to irc", null);
-		ircClient.getEventManager().registerEventListener(this);
-	}
-
-	// irc - > osrs
-	@Handler
-	public void privmsg(org.kitteh.irc.client.library.event.user.PrivateMessageEvent event) {
-		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "IRC PM RECIEVED: " + event.getMessage(), null);
+		// TODO: this starts the client on plugin load rn, have it connect on login and dc on logout maybe?
+		startIRC();
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "disconnected from irc", null);
-		ircClient.shutdown();
-
+		stopIRC();
 	}
-	// osrs -> irc
+
+	private void startIRC() {
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Starting Up", null);
+
+		// Start irc client (insecure conn ONLY)
+		ircClient = org.kitteh.irc.client.library.Client.builder().nick(config.ircNick()).server().port(config.ircPort(), org.kitteh.irc.client.library.Client.Builder.Server.SecurityType.INSECURE).host(config.ircHostname()).then().buildAndConnect();
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Connected To IRC", null);
+
+
+		// join a channel
+		ircClient.addChannel(config.ircChannel());
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Joined " + config.ircChannel(), null);
+		ircClient.getEventManager().registerEventListener(this);
+	}
+
+	private void stopIRC() {
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "disconnecting from irc", null);
+		ircClient.shutdown();
+	}
+
+
+
+	// fires on osrs chat message, sends messages osrs -> irc
 	@Subscribe
 	public void onChatMessage(ChatMessage event) {
 		switch (event.getType()) {
 			case PUBLICCHAT:
-				ircClient.sendMessage("#rs_memes_ayy_lmao", "[Public Chat] <" + event.getName() + "> " + event.getMessage());
+				if (config.syncPublicChat())
+					ircClient.sendMessage("#rs_memes_ayy_lmao", "[Public Chat] <" + event.getName() + "> " + event.getMessage());
+				break;
+			case CLAN_CHAT:
+				if (config.syncClanChat())
+					ircClient.sendMessage("#rs_memes_ayy_lmao", "[Clan Chat] <" + event.getName() + "> " + event.getMessage());
+				break;
+			case FRIENDSCHAT:
+				if (config.syncFriendsChat())
+					ircClient.sendMessage("#rs_memes_ayy_lmao", "[Friends Chat] <" + event.getName() + "> " + event.getMessage());
 				break;
 		}
 	}
+
+	// restart irc client on config change
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event) {
+		// if config changed was not of our plugin
+		if (!event.getGroup().equals("osrsircPlugin"))
+			return; // don't reconnect
+		// else, disconnect and reload irc client
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "IRC Config Changed, Reconnecting", null);
+		// disconnect
+		ircClient.shutdown();
+		// reconstruct client with latest cfg values
+		startIRC();
+	}
+
+	//
+//	// fires on IRC PM, irc - > osrs
+//	@Handler
+//	public void privmsg(org.kitteh.irc.client.library.event.user.PrivateMessageEvent event) {
+//		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "IRC PM RECIEVED: " + event.getMessage(), null);
+//	}
 
 
 	@Provides
